3dab1e7ba682971e2c3476b3f2054ae8d1f46ddf
Sina <sina.frensch@uni-ulm.de>
2025-05-16 16:20:06 +0200

    Implement class SpreadCalculator to calculate smallest difference and add unit tests

--- src/test/java/de/exxcellent/challenge/SpreadCalculatorTest.java
+++ src/test/java/de/exxcellent/challenge/SpreadCalculatorTest.java
@@ -1,5 +1,106 @@
 package de.exxcellent.challenge;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
 public class SpreadCalculatorTest {
 
+    @Test
+    public void getIdxWithSmallestDiff_whenValidColumns_thenReturnsValidIdx() {
+
+        List<List<String>> columns = Arrays.asList(
+                Arrays.asList("88", "59"),
+                Arrays.asList("79", "63"),
+                Arrays.asList("77", "55"),
+                Arrays.asList("77", "59"),
+                Arrays.asList("90", "66"),
+                Arrays.asList("81", "61"),
+                Arrays.asList("73", "57"),
+                Arrays.asList("75", "54"),
+                Arrays.asList("86", "32"),
+                Arrays.asList("84", "64"),
+                Arrays.asList("91", "59"),
+                Arrays.asList("88", "73"),
+                Arrays.asList("70", "59"),
+                Arrays.asList("61", "59"),
+                Arrays.asList("64", "55"),
+                Arrays.asList("79", "59"),
+                Arrays.asList("81", "57"),
+                Arrays.asList("82", "52"),
+                Arrays.asList("81", "61"),
+                Arrays.asList("84", "57"),
+                Arrays.asList("86", "59"),
+                Arrays.asList("90", "64"),
+                Arrays.asList("90", "68"),
+                Arrays.asList("90", "77"),
+                Arrays.asList("90", "72"),
+                Arrays.asList("97", "64"),
+                Arrays.asList("91", "72"),
+                Arrays.asList("84", "68"),
+                Arrays.asList("88", "66"),
+                Arrays.asList("90", "45"));
+
+        Integer rowIdx = SpreadCalculator.getIdxWithSmallestDiff(columns);
+        assertEquals(13, rowIdx);
+    }
+
+    @Test
+    public void getIdxWithSmallestDiff_whenOnlyOneRow_thenReturnsIdxZero() {
+
+        List<List<String>> columns = Arrays.asList(
+                Arrays.asList("88", "89"));
+
+        Integer rowIdx = SpreadCalculator.getIdxWithSmallestDiff(columns);
+        assertEquals(0, rowIdx);
+    }
+
+    @Test
+    public void getIdxWithSmallestDiff_whenMultipleMinSpreads_thenReturnsFirstOccurrence() {
+        List<List<String>> columns = Arrays.asList(
+                Arrays.asList("80", "70"),
+                Arrays.asList("81", "71"),
+                Arrays.asList("90", "75"));
+
+        Integer rowIdx = SpreadCalculator.getIdxWithSmallestDiff(columns);
+        assertEquals(0, rowIdx);
+    }
+
+    @Test
+    public void getIdxWithSmallestDiff_whenEmptyList_thenReturnsIdxMinusOne() {
+
+        List<List<String>> columns = Arrays.asList();
+
+        Integer rowIdx = SpreadCalculator.getIdxWithSmallestDiff(columns);
+        assertEquals(-1, rowIdx);
+    }
+
+    @Test
+    public void getIdxWithSmallestDiff_whenRowSizeTooSmall_thenSkipRow() {
+
+        List<List<String>> columns = Arrays.asList(
+                Arrays.asList("88", "86"),
+                Arrays.asList("79"),
+                Arrays.asList("77", "55"),
+                Arrays.asList("77", "76"));
+
+        Integer rowIdx = SpreadCalculator.getIdxWithSmallestDiff(columns);
+        assertEquals(3, rowIdx);
+    }
+
+    @Test
+    public void getIdxWithSmallestDiff_whenInvalidValues_thenSkipRows() {
+
+        List<List<String>> columns = Arrays.asList(
+                Arrays.asList(".", "86"),
+                Arrays.asList("79", "a"),
+                Arrays.asList("77", "55"),
+                Arrays.asList("77", ""));
+
+        Integer rowIdx = SpreadCalculator.getIdxWithSmallestDiff(columns);
+        assertEquals(2, rowIdx);
+    }
 }
