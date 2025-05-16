b95f006e4a8c95e96b71947418e03de9bec303fa
Sina <sina.frensch@uni-ulm.de>
2025-05-16 15:02:14 +0200

    Implement class DataFile to represent data file structure and accessors

--- src/main/java/de/exxcellent/challenge/DataFile.java
+++ src/main/java/de/exxcellent/challenge/DataFile.java
@@ -1,5 +1,45 @@
 package de.exxcellent.challenge;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * This class represents a data file with its path, a label column and columns
+ * to compare.
+ */
 public class DataFile {
 
+    private String path;
+    private String labelCol;
+    private List<String> colsToCompare;
+
+    public DataFile(String path, String labelCol, List<String> colsToCompare) {
+        this.path = path;
+        this.labelCol = labelCol;
+        this.colsToCompare = colsToCompare;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getLabelCol() {
+        return labelCol;
+    }
+
+    public void setLabelCol(String labelCol) {
+        this.labelCol = labelCol;
+    }
+
+    public List<String> getColsToCompare() {
+        return colsToCompare;
+    }
+
+    public void setColsToCompare(ArrayList<String> colsToCompare) {
+        this.colsToCompare = colsToCompare;
+    }
 }
