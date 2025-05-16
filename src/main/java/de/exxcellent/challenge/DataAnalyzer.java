752ae71a685523a2bbeaf55472375d459b2be2c1
Sina <sina.frensch@uni-ulm.de>
2025-05-16 16:54:52 +0200

    Implement class DataAnalyzer to find label with smallest spread and add unit tests

--- src/main/java/de/exxcellent/challenge/DataAnalyzer.java
+++ src/main/java/de/exxcellent/challenge/DataAnalyzer.java
@@ -1,5 +1,43 @@
 package de.exxcellent.challenge;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * This class is responsible for analyzing data files.
+ */
 public class DataAnalyzer {
 
+    /**
+     * Calcualtes the row with the smallest spread between two specific columns in
+     * the given data file and returns the label of that row.
+     *
+     * @param file The data file to analyze.
+     * @return The label of the row with the smallest spread.
+     * @throws IOException If an error occurs while reading the file.
+     */
+    public static String findLabelWithSmallestSpread(DataFile file) throws IOException {
+        /* Extract the label column and the columns to compare */
+        List<List<String>> columnList = DataReader.extractColumns(file);
+
+        /*
+         * Extract the last two columns, as the label column is not relevant for the
+         * calculation
+         */
+        List<List<String>> columnsToCompare = new ArrayList<>();
+        for (List<String> row : columnList) {
+            columnsToCompare.add(row.subList(1, 3));
+        }
+
+        /* Calculate the index of the row with the smallest spread */
+        int rowIdx = SpreadCalculator.getIdxWithSmallestDiff(columnsToCompare);
+        if (rowIdx == -1) {
+            System.out.println("File is empty or contains no valid data.");
+            return null;
+        }
+
+        /* Return the label */
+        return columnList.get(rowIdx).get(0);
+    }
 }
--- src/test/java/de/exxcellent/challenge/DataAnalyzerTest.java
+++ src/test/java/de/exxcellent/challenge/DataAnalyzerTest.java
@@ -1,5 +1,49 @@
 package de.exxcellent.challenge;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mockStatic;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+
 public class DataAnalyzerTest {
 
+    private static final String BASE_PATH = "src/test/resources/de/exxcellent/challenge/";
+
+    @Test
+    public void findLabelWithSmallestSpread_whenValidFile_thenReturnsValidLabel() throws Exception {
+        DataFile weatherFile = new DataFile(BASE_PATH + "/weatherValid.csv", "Day",
+                Arrays.asList("MxT", "MnT"));
+
+        String label = DataAnalyzer.findLabelWithSmallestSpread(weatherFile);
+        assertEquals("14", label);
+    }
+
+    @Test
+    public void findLabelWithSmallestSpread_whenEmptyFile_thenPropagatesIOException() throws IOException {
+        DataFile file = new DataFile("fake/path.csv", "Day", List.of("MxT", "MnT"));
+
+        try (MockedStatic<DataReader> mocked = mockStatic(DataReader.class)) {
+            mocked.when(() -> DataReader.extractColumns(file))
+                    .thenThrow(new IOException("Simulates IO-Exception"));
+
+            assertThrows(IOException.class,
+                    () -> DataAnalyzer.findLabelWithSmallestSpread(file),
+                    "Expected IOException to be propagated from extractColumns");
+        }
+    }
+
+    @Test
+    public void findLabelWithSmallestSpread_whenOnlyHeaderRow_thenReturnsNull() throws IOException {
+        DataFile weatherFile = new DataFile(BASE_PATH + "/weatherOnlyHeader.csv", "Day",
+                Arrays.asList("MxT", "MnT"));
+
+        String label = DataAnalyzer.findLabelWithSmallestSpread(weatherFile);
+        assertEquals(null, label);
+    }
 }
